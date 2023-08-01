89e54a4e5532db717ec1628ef2b6a351c64630d7
ingdanielb <danielb2889@misena.edu.co>
2023-08-01 07:28:27 -0500

    correccion metodo contratar

--- dart_poo/bin/agencia.dart
+++ dart_poo/bin/agencia.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'jugador.dart';
+
 class Agencia {
   String nombre;
   int idJugador;
@@ -10,9 +12,12 @@ class Agencia {
   static List<Agencia> jugadoresContratados = [];
 
   static contratarJugador(int id){
+    print('Ingree nombre de la agencia');
     String nombre = stdin.readLineSync()!;
+    print('Ingree el salario');
     int salario= int.parse(stdin.readLineSync()!);
     jugadoresContratados.add(Agencia(nombre, id, salario));
+    print('Jugador contratado');
   }
 
   static jugadoresContratos(){
@@ -20,7 +25,8 @@ class Agencia {
       print('No hay jugadores contratados');
     } else {
       for (var j in jugadoresContratados) {
-        print('Nombre: ${j.nombre} - Id: ${j.idJugador} - Salario: ${j.salario}');
+
+        print('Nombre: ${j.nombre} - Id: ${j.idJugador} ${Jugador.jugadores.firstWhere((element) => element.id == j.idJugador)}- Salario: ${j.nombre}');
       }      
     }
   }
