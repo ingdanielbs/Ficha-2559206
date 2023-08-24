b169dae75436a7c1424dde9304ea72b573b8be03
ingdanielb <danielb2889@misena.edu.co>
2023-08-24 09:40:43 -0500

    doctor reg finish

--- medical_help/lib/presentation/screens/doctor_registration_screen.dart
+++ medical_help/lib/presentation/screens/doctor_registration_screen.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:medical_help/datos/doctor_list.dart';
+import 'package:medical_help/dominio/models/doctor.dart';
+import 'package:medical_help/presentation/screens/home_screen.dart';
 import 'package:medical_help/presentation/widgets/appbar_menu.dart';
 
 class DoctorRegistrationScreen extends StatefulWidget {
@@ -38,73 +41,91 @@ class _DoctorRegistrationScreenState extends State<DoctorRegistrationScreen> {
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Form(
-            key: _formKey,
-            child: ListView(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Nombre'),
-                  controller: _nameController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingresa un nombre';
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre'),
+                controller: _nameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa un nombre';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Apellido'),
+                controller: _lastnameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa un apellido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Télefono'),
+                controller: _phoneController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa un teléfono';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Especialidad'),
+                controller: _specialityController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa una especialidad';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: _pickImage,
+                child: const Text('Selecciona foto'),
+              ),
+              if (photo.isNotEmpty)
+                /* Image.network(photo) */ /* Para navegador */
+                Image.file(
+                  File(photo),
+                ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      var newDoctor = Doctor(
+                          name: _nameController.text,
+                          lastName: _lastnameController.text,
+                          phone: _phoneController.text,
+                          speciality: _specialityController.text,
+                          photo: photo);
+                      doctores.add(newDoctor);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ),
+                      );
+                      /* for (var d in doctores) {
+                        print('${d.name}, ${d.lastName}, ${d.phone}, ${d.speciality}, ${d.photo}');
+                      } */
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Registro exitoso')),
+                      );
                     }
-                    return null;
                   },
+                  child: const Text('Registrar'),
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Apellido'),
-                  controller: _lastnameController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingresa un apellido';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: 'Télefono'),
-                  controller: _phoneController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingresa un teléfono';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Especialidad'),
-                  controller: _specialityController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingresa una especialidad';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: _pickImage,
-                  child: const Text('Selecciona foto'),
-                ),
-                if (photo.isNotEmpty)
-                  Image.file(
-                    File(photo),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Registro exitoso')),
-                        );
-                      }
-                    },
-                    child: const Text('Registrar'),
-                  ),
-                ),
-              ],
-            )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
